2abbeaf4bf97ed97fc726eb2326c1c813f95c838
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-03 18:17:36 +1100

    Added dbcontext

---
+++ MyCantinaCore/src/MyCantinaCore/DataAccess/Models/MyCantinaCoreDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyCantinaCore.DataAccess.Models
+{
+    public class MyCantinaCoreDbContext : DbContext
+    {
+        public DbSet<Bottle> Bottles { get; set; }
+        public DbSet<Consumer> Consumers { get; set; }
+        public DbSet<ConsumerBottle> ConsumerBottles { get; set; }
+        public DbSet<GrapeVariety> GrapeVarieties { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ConsumerBottle>()
+                .Property(b => b.PricePaid)
+                .HasColumnType("money");
+
+            modelBuilder.Entity<ConsumerBottle>()
+                .Property(b => b.Owned)
+                .HasColumnType("bit");
+
+            modelBuilder.Entity<ConsumerBottle>()
+                .HasKey(cb => new { cb.BottleId, cb.ConsumerId });
+
+            modelBuilder.Entity<Review>()
+                .HasKey(r => new { r.BottleId, r.ConsumerId });
+        }
+    }
+}
