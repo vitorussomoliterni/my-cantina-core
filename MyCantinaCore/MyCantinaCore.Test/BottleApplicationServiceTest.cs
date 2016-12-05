5f7261800f687d4497905d378b66e20d61c67dcb
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-05 15:17:40 +1100

    Added test method for add bottle

--- MyCantinaCore/MyCantinaCore.Test/BottleApplicationServiceTest.cs
+++ MyCantinaCore/MyCantinaCore.Test/BottleApplicationServiceTest.cs
@@ -7,6 +7,7 @@ using Xunit;
 using MyCantinaCore.Services;
 using MyCantinaCore.DataAccess.Models;
 using Microsoft.Extensions.DependencyInjection;
+using MyCantinaCore.Commands.Bottle;
 
 namespace MyCantinaCore.Test
 {
@@ -27,6 +28,83 @@ namespace MyCantinaCore.Test
                 .UseInternalServiceProvider(serviceProvider);
 
             return builder.Options;
+        }
+
+        private static Bottle CreateNewBotle()
+        {
+            var bottle = new Bottle()
+            {
+                Name = "Terra rossa",
+                Year = "2014",
+                Producer = "Producer of Fine Wine",
+                Description = "A robust red, for pasta lovers",
+                WineType = "Red",
+                Region = "Basilicata",
+                Country = "Italy",
+                AverageRating = 4.8
+            };
+
+            return bottle;
+        }
+
+        [Fact]
+        private async static Task AddBottleTest_ShouldAddNewBottleIntoDb()
+        {
+            var options = CreateNewContextOptions();
+
+            using (var context = new MyCantinaCoreDbContext(options))
+            {
+                // Fixture
+                var service = new BottleApplicationService(context);
+
+                var command = new AddBottleCommand()
+                {
+                    Name = "Terra rossa",
+                    Year = "2014",
+                    Producer = "Producer of Fine Wine",
+                    Description = "A robust red, for pasta lovers",
+                    WineType = "Red",
+                    Region = "Basilicata",
+                    Country = "Italy"
+                };
+
+                command.GrapeVarietyIds = new List<int>() { 1, 2 };
+
+                await context.GrapeVarieties.AddAsync(new GrapeVariety()
+                {
+                    Name = "Aglianico",
+                    Colour = "Red"
+                });
+
+                await context.GrapeVarieties.AddAsync(new GrapeVariety()
+                {
+                    Name = "Merlot",
+                    Colour = "Red"
+                });
+
+                await context.SaveChangesAsync();
+
+                var expectedBottle = new Bottle()
+                {
+                    Id = 1,
+                    Name = "Terra Rossa",
+                    Year = "2014",
+                    Producer = "Producer of Fine Wine",
+                    Description = "A robust red, for pasta lovers",
+                    WineType = "Red",
+                    Region = "Vulture",
+                    Country = "Italy",
+                    AverageRating = 0
+                };
+
+                // SUT
+                await service.AddBottle(command);
+
+                var actualBottle = await context.Bottles.FirstOrDefaultAsync();
+
+                // Verify Outcome
+                Assert.NotEmpty(context.Bottles);
+            }
         }
     }
 }
