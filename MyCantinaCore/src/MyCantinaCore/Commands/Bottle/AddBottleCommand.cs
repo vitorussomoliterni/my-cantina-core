c6cfdb674c21d26582aa2d28a9027dda16d4fd6e
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-05 12:45:02 +1100

    Added service and command

---
+++ MyCantinaCore/src/MyCantinaCore/Commands/Bottle/AddBottleCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MyCantinaCore.Commands.Bottle
+{
+    public class AddBottleCommand
+    {
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Producer { get; set; }
+        public string Description { get; set; }
+        public string WineType { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public List<int> GrapeVarietyIds { get; set; }
+    }
+}
---
+++ MyCantinaCore/src/MyCantinaCore/Services/BottleApplicationService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyCantinaCore.DataAccess.Models;
+using MyCantinaCore.Commands.Bottle;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyCantinaCore.Services
+{
+    public class BottleApplicationService
+    {
+        private readonly MyCantinaCoreDbContext _context;
+
+        public BottleApplicationService(MyCantinaCoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Bottle> AddBottle(AddBottleCommand command)
+        {
+            var bottle = new Bottle()
+            {
+                Name = command.Name,
+                Year = command.Year,
+                Producer = command.Producer,
+                Description = command.Description,
+                WineType = command.WineType,
+                Region = command.Region,
+                Country = command.Country,
+                AverageRating = 0
+            };
+
+            var grapeVarieties = new List<GrapeVariety>();
+
+            foreach (var id in command.GrapeVarietyIds)
+            {
+                var grapeVariety = await _context.GrapeVarieties.FirstOrDefaultAsync(gv => gv.Id == id);
+                var bottleGrapeVariety = new BottleGrapeVariety()
+                {
+                    Bottle = bottle,
+                    GrapeVariety = grapeVariety
+                };
+
+                bottle.BottleGrapeVarieties.Add(bottleGrapeVariety);
+            }
+
+            await _context.Bottles.AddAsync(bottle);
+
+            await _context.SaveChangesAsync();
+
+            return bottle;
+        }
+    }
+}
