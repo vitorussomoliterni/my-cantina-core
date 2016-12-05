520d3bea1ab0cbfc4ae985acaeddb32be7d45a1e
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-05 15:08:51 +1100

    Added constructor to initialise list

--- MyCantinaCore/src/MyCantinaCore/Commands/Bottle/UpdateBottleCommand.cs
+++ MyCantinaCore/src/MyCantinaCore/Commands/Bottle/UpdateBottleCommand.cs
@@ -2,13 +2,14 @@
 
 namespace MyCantinaCore.Commands.Bottle
 {
-    public class AddBottleCommand
+    public class UpdateBottleCommand
     {
-        public AddBottleCommand()
+        public UpdateBottleCommand()
         {
             GrapeVarietyIds = new List<int>();
         }
 
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Year { get; set; }
         public string Producer { get; set; }
