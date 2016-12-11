c40d1133bcb2781c9847a92ee1aefa12e941e843
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-11 18:05:44 +1100

    Completed consumer bottle service

---
+++ MyCantinaCore/src/MyCantinaCore/Commands/ConsumerBottle/ConsumerBottleCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyCantinaCore.Commands.ConsumerBottle
+{
+    public class ConsumerBottleCommand
+    {
+        public int ConsumerId { get; set; }
+        public int BottleId { get; set; }
+        public DateTime DateAcquired { get; set; }
+        public DateTime? DateOpened { get; set; }
+        public int Qty { get; set; }
+        public bool Owned { get; set; }
+        public double? PricePaid { get; set; }
+    }
+}
--- MyCantinaCore/src/MyCantinaCore/Services/ConsumerBottleApplicationService.cs
+++ MyCantinaCore/src/MyCantinaCore/Services/ConsumerBottleApplicationService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyCantinaCore.Commands.ConsumerBottle;
+using MyCantinaCore.DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,77 @@ namespace MyCantinaCore.Services
 {
     public class ConsumerBottleApplicationService
     {
+        private readonly MyCantinaCoreDbContext _context;
+
+        public ConsumerBottleApplicationService(MyCantinaCoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ConsumerBottle> AddConsumerBottle(ConsumerBottleCommand command)
+        {
+            var consumerBottle = new ConsumerBottle()
+            {
+                ConsumerId = command.ConsumerId,
+                BottleId = command.BottleId,
+                DateAcquired = command.DateAcquired,
+                DateOpened = command.DateOpened,
+                Qty = command.Qty,
+                Owned = command.Owned,
+                PricePaid = command.PricePaid
+            };
+
+            await _context.ConsumerBottles.AddAsync(consumerBottle);
+            await _context.SaveChangesAsync();
+
+            return consumerBottle;
+        }
+
+        public async Task<ConsumerBottle> UpdateConsumerBottle(ConsumerBottleCommand command)
+        {
+            var consumerBottle = _context.ConsumerBottles.FirstOrDefault(cb => cb.BottleId == command.BottleId && cb.ConsumerId == command.ConsumerId);
+
+            if (consumerBottle == null)
+                throw new InvalidOperationException("No consumer bottle found");
+
+            consumerBottle.DateAcquired = command.DateAcquired;
+            consumerBottle.DateOpened = command.DateOpened;
+            consumerBottle.Qty = command.Qty;
+            consumerBottle.Owned = command.Owned;
+            consumerBottle.PricePaid = command.PricePaid;
+
+            _context.ConsumerBottles.Update(consumerBottle);
+            await _context.SaveChangesAsync();
+
+            return consumerBottle;
+        }
+
+        public async Task DeleteConsumerBottle(int consumerId, int bottleId)
+        {
+            var consumerBottle = _context.ConsumerBottles.FirstOrDefault(cb => cb.BottleId == bottleId && cb.ConsumerId == consumerId);
+
+            if (consumerBottle == null)
+                throw new InvalidOperationException("No consumer bottle found");
+
+            _context.ConsumerBottles.Remove(consumerBottle);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<ConsumerBottle> GetAllConsumerBottles(int consumerId)
+        {
+            var consumerBottles = _context.ConsumerBottles.Where(cb => cb.ConsumerId == consumerId);
+
+            return consumerBottles;
+        }
+
+        public ConsumerBottle GetConsumerBottle(int bottleId, int consumerId)
+        {
+            var consumerBottle = _context.ConsumerBottles.FirstOrDefault(cb => cb.BottleId == bottleId && cb.ConsumerId == consumerId);
+
+            if (consumerBottle == null)
+                throw new InvalidOperationException("No consumer bottle found");
+
+            return consumerBottle;
+        }
     }
 }
