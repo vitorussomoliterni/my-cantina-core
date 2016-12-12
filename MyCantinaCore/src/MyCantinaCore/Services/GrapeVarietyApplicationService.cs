d4894d77ea8a2dc46e35399039b06a575802bc22
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-12 20:21:04 +1100

    Added method for grape variety service

---
+++ MyCantinaCore/src/MyCantinaCore/Services/GrapeVarietyApplicationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyCantinaCore.DataAccess.Models;
+
+namespace MyCantinaCore.Services
+{
+    public class GrapeVarietyApplicationService
+    {
+        private readonly MyCantinaCoreDbContext _context;
+
+        public GrapeVarietyApplicationService(MyCantinaCoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GrapeVariety> AddGrapeVariety(string name, string colour)
+        {
+            var grapeVariety = new GrapeVariety()
+            {
+                Name = name,
+                Colour = colour
+            };
+
+            await _context.GrapeVarieties.AddAsync(grapeVariety);
+            await _context.SaveChangesAsync();
+
+            return grapeVariety;
+        }
+    }
+}
