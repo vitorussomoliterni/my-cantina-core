40806ca7f02a62218dc33721c094295433cb9299
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-13 16:58:25 +1100

    Finished service class for grape veriety

--- MyCantinaCore/src/MyCantinaCore/Services/GrapeVarietyApplicationService.cs
+++ MyCantinaCore/src/MyCantinaCore/Services/GrapeVarietyApplicationService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MyCantinaCore.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyCantinaCore.Services
 {
@@ -26,6 +27,38 @@ namespace MyCantinaCore.Services
             await _context.GrapeVarieties.AddAsync(grapeVariety);
             await _context.SaveChangesAsync();
 
+            return grapeVariety;
+        }
+
+        public async Task<GrapeVariety> UpdateGrapeVariety(int id, string name, string colour)
+        {
+            var grapeVariety = await _context.GrapeVarieties.FirstOrDefaultAsync(gv => gv.Id == id);
+
+            if (grapeVariety == null)
+                throw new InvalidOperationException($"No grape variety found for id {id}");
+
+            grapeVariety.Name = name;
+            grapeVariety.Colour = colour;
+
+            await _context.SaveChangesAsync();
+
+            return grapeVariety;
+        }
+
+        public IQueryable<GrapeVariety> GetAllGrapeVarieties()
+        {
+            var grapeVarieties = _context.GrapeVarieties;
+
+            return grapeVarieties;
+        }
+
+        public async Task<GrapeVariety> GetGrapeVariety(int id)
+        {
+            var grapeVariety = await _context.GrapeVarieties.FirstOrDefaultAsync(gv => gv.Id == id);
+
+            if (grapeVariety == null)
+                throw new InvalidOperationException($"No grape variety found for id {id}");
+            
             return grapeVariety;
         }
     }
