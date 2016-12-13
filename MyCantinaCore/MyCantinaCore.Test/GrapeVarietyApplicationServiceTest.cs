ca9f03eadb8085c0a40cf69fd4298ddd6cf41f51
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-13 17:09:39 +1100

    Added test for grape variety service

---
+++ MyCantinaCore/MyCantinaCore.Test/GrapeVarietyApplicationServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using MyCantinaCore.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyCantinaCore.Services;
+
+namespace MyCantinaCore.Test
+{
+    public class GrapeVarietyApplicationServiceTest
+    {
+        private static DbContextOptions<MyCantinaCoreDbContext> CreateNewContextOptions()
+        {
+            // Create a fresh service provider, and therefore a fresh 
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            var builder = new DbContextOptionsBuilder<MyCantinaCoreDbContext>();
+            builder.UseInMemoryDatabase()
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        private static GrapeVariety CreateNewGrapeVariety()
+        {
+            var grapeVariety = new GrapeVariety()
+            {
+                Name = "Aglianico",
+                Colour = "Red"
+            };
+
+            return grapeVariety;
+        }
+
+        [Fact]
+        public static async Task AddGrapeVarietyTest_ShouldAddGrapeVarietyIntoDb()
+        {
+            var options = CreateNewContextOptions();
+
+            using (var context = new MyCantinaCoreDbContext(options))
+            {
+                // Fixture
+                var service = new GrapeVarietyApplicationService(context);
+
+                var expectedGrapeVariety = CreateNewGrapeVariety();
+
+                // S.U.T.
+                await service.AddGrapeVariety(expectedGrapeVariety.Name, expectedGrapeVariety.Colour);
+
+                var actualGrapeVariety = await context.GrapeVarieties.FirstOrDefaultAsync();
+
+                // Verify Outcome
+                Assert.NotEmpty(context.GrapeVarieties);
+                Assert.Equal(1, context.GrapeVarieties.Count());
+                Assert.Equal(1, actualGrapeVariety.Id);
+                Assert.Equal(expectedGrapeVariety.Name, actualGrapeVariety.Name);
+                Assert.Equal(expectedGrapeVariety.Colour, actualGrapeVariety.Colour);
+            }
+        }
+    }
+}
