472ddc10f03b75bc87a2fb4f115613420f934fda
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-15 09:59:29 +1100

    Added api controller for grape variety

---
+++ MyCantinaCore/src/MyCantinaCore.UI/Controllers/GrapeVarietyController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCantinaCore.Services;
+
+namespace MyCantinaCore.UI.Controllers
+{
+    [Route("api/[controller]")]
+    public class GrapeVarietyController : Controller
+    {
+        private readonly GrapeVarietyApplicationService _grapeVarietyService;
+
+        public GrapeVarietyController(GrapeVarietyApplicationService grapeVarietyService)
+        {
+            _grapeVarietyService = grapeVarietyService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGrapeVariety(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            try
+            {
+                var grapeVariety = await _grapeVarietyService.GetGrapeVariety(id.Value);
+                return new ObjectResult(grapeVariety);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
