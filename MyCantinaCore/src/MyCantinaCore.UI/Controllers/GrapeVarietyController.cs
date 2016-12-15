d984c8335cb8cea4d12c10d45fc2a2e630838695
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-15 20:32:07 +1100

    Added more methods

--- MyCantinaCore/src/MyCantinaCore.UI/Controllers/GrapeVarietyController.cs
+++ MyCantinaCore/src/MyCantinaCore.UI/Controllers/GrapeVarietyController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyCantinaCore.Services;
 using MyCantinaCore.DataAccess.Models;
+using MyCantinaCore.UI.ViewModels;
 
 namespace MyCantinaCore.UI.Controllers
 {
@@ -32,6 +33,54 @@ namespace MyCantinaCore.UI.Controllers
             catch (Exception ex)
             {
                 return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var grapeVarieties = _grapeVarietyService.GetAllGrapeVarieties().ToList();
+                return new ObjectResult(grapeVarieties);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            try
+            {
+                await _grapeVarietyService.DeleteGrapeVariety(id.Value);
+                return new NoContentResult();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] GrapeVarietyCreateViewModel model)
+        {
+            if (model == null)
+                return BadRequest();
+
+            try
+            {
+                var grapeVariety = await _grapeVarietyService.AddGrapeVariety(model.Name, model.Colour);
+                return new ObjectResult(grapeVariety);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
         }
     }
