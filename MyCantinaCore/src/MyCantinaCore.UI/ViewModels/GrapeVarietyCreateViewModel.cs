36e6788a6b38569db16dd85dfcf78d2bf85d759e
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-15 20:31:54 +1100

    Added view model for create

---
+++ MyCantinaCore/src/MyCantinaCore.UI/ViewModels/GrapeVarietyCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyCantinaCore.UI.ViewModels
+{
+    public class GrapeVarietyCreateViewModel
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+    }
+}
