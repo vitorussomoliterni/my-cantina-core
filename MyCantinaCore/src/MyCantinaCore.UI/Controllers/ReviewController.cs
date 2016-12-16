df397b5bc9a4a2f21d54d8a75938aa512c27f8a6
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-12-16 21:09:30 +1100

    Added api controller class for reviews

---
+++ MyCantinaCore/src/MyCantinaCore.UI/Controllers/ReviewController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyCantinaCore.DataAccess.Models;
+using MyCantinaCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCantinaCore.UI.Controllers
+{
+    [Route("api")]
+    public class ReviewController : Controller
+    {
+        private readonly ReviewApplicationService _reviewService;
+
+        public ReviewController(ReviewApplicationService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        // GET: api / Consumers / Id / Reviews
+        [HttpGet("Consumers/{id}/Reviews")]
+        public async Task<IActionResult> GetReview(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            try
+            {
+                var result = await _reviewService.GetAllReviewsByConsumerId(id.Value).ToListAsync();
+
+                return new ObjectResult(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
